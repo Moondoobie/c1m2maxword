568987f2f3979c6c7714433c8c819dacc9e28247
Valdyn Hunt <valdyn@mac.com>
2016-03-14 16:25:30 -0700

    Added variables and methods.

--- module2_assignment.rb
+++ module2_assignment.rb
@@ -20,22 +20,31 @@ class LineAnalyzer
   #Implement the initialize() method to:
   def initialize (content, line_number)
     #* take in a line of text and line number
-    line = content
-    line_number = line_number || 1
     #* initialize the content and line_number attributes
-
+    line = content
+    line_number = 1
+    
 
     #* call the calculate_word_frequency() method.
-    calculate_word_frequency()
+    calculate_word_frequency(line)
   end
 
 
-  #Implement the calculate_word_frequency() method to
-  def calculate_word_frequency
+  def calculate_word_frequency (line)
       #* calculate the maximum number of times a single word appears within
       #  provided content and store that in the highest_wf_count attribute.
       #* identify the words that were used the maximum number of times and
       #  store that in the highest_wf_words attribute.
+
+      word_frequency = Hash.new(0) 
+ 
+      line.split.each do |word| 
+        word_frequency[word.downcase] += 1 
+      end 
+
+      p word_frequency # => {"chicka" => 2, "boom" => 2}
+      highest_wf_count = word_frequency.values.max
+      highest_wf_words << word_frequency.word(highest_wf_count)
   end
 end
 
@@ -51,12 +60,24 @@ class Solution
   # Implement the following methods in the Solution class.
   #* initialize() - initialize analyzers as an empty array.
   def initialize
+    @@analyzers = []
 
   end
 
   #* analyze_file() - processes 'test.txt' intro an array of LineAnalyzers and stores them in analyzers.
   def analyze_file
+    line_number = 0
     #* Read the 'test.txt' file in lines 
+    if File.exist? 'test.txt'
+
+    File.foreach( 'test.txt' ) do |line|
+      p line.chomp
+      line_number ++
+      LineAnalyzer.initialize(line, line_number)
+      analyzers << line
+    end
+
+  end
     #* Create an array of LineAnalyzers for each line in the file
   end
 
@@ -75,6 +96,11 @@ class Solution
   #  highest_count_words_across_lines in the specified format
   def print_highest_word_frequency_across_lines
     #* print the values of objects in highest_count_words_across_lines in the specified format
+    # "#{one} multiplied by #{two} equals #{one * two}" 
   end
   
 end
+
+# p line.split # array of words in line
+
+# hash.key(value) => key
